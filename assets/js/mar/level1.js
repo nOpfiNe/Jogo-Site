9bd87b1ee62fcd8197b2b18fcb000f237af7326f
Bruna Moura <104026303+nOpfiNe@users.noreply.github.com>
2023-06-05 16:21:31 -0300

    Add files via upload

---
+++ assets/js/mar/level1.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const gM = document.querySelector(".imgClick");
+const iGM = document.querySelector(".imgClick img");
+
+const menuSize = '260px';
+
+let open = true;
+
+
+document.querySelector('.btnMenu').addEventListener('click', e => {
+    open = !open;
+    toggleMenu();
+})
+
+document.querySelector('.btnClose').addEventListener('click', e => {
+    open = false;
+
+    toggleMenu();
+})
+
+function toggleMenu() {
+    if (open) {
+        document.querySelector('#menu').style.marginLeft = 0;
+        return;
+    }
+
+    document.querySelector('#menu').style.marginLeft = `-${menuSize}`;
+}
+
+// Grab ID of audio player
+var podcastAudio = document.getElementById('podcast-audio');
+
+// Grab ID of play button
+var playBtn = document.getElementById('podcast-play');
+
+// Grab ID of pause button
+var pauseBtn = document.getElementById('podcast-pause');
+
+// Play audio & show pause btn
+var playShow = function() {
+  podcastAudio.play();
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "inline-block";
+};
+
+// Pause audio & show play btn
+var pauseShow = function() {
+  podcastAudio.pause();
+  playBtn.style.display = "inline-block";
+  pauseBtn.style.display = "none";
+};
